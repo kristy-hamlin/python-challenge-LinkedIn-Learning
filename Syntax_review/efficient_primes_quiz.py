83b045764284e04547513fdc71bc4a8f4fc140cd
Kristy Hamlin <khamlin2023@gmail.com>
2024-11-23 11:25:56 -0500

    Completed Primes Challenge

---
+++ Syntax_review/efficient_primes_quiz.py
@@ -0,0 +1,22 @@
+#Chapter 5 Challenge: Efficient Primes
+#Write a function called allPrimesUpTo() which returns a list of all primes
+#up to the given input number.
+
+#Input: num - a whole positive integer
+#Output: list - a list of all integer primes less than num
+
+#Make the function efficient by using the list of previously found primes
+#to test for the current prime instead of testing all possible factors. 
+
+def allPrimesUpTo(num):
+    primes = [2]
+    for x in range(2, num + 1):
+        for prime in primes:
+            if x % prime == 0:
+                break
+        else:
+            primes.append(x)
+    return primes
+
+
+print(allPrimesUpTo(11))    
