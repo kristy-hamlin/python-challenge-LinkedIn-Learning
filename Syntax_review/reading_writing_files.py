6986b94bcf13bce92c5861d6eaac6b548d0fcbdb
Kristy Hamlin <khamlin2023@gmail.com>
2024-12-04 18:29:37 -0500

    adding new text file for testing

--- Syntax_review/reading_writing_files.py
+++ Syntax_review/reading_writing_files.py
@@ -5,7 +5,7 @@ def readingDemo1():
     # The open() function is used to open a file. The first positional argument is
     # the relative path to the file, the second argument in this case is a string, 'r',
     # which signifies we will be only reading the file, not writing it:
-    f = open('Holiday_glow.txt', 'r')
+    f = open('text.txt', 'r')
     print(f)
     # This should print the f object, which is a file object.
 
