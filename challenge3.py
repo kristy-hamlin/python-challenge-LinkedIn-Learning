0caed2dea030aa1df87530091e2d1db49ecd421e
Kristy Hamlin <khamlin2023@gmail.com>
2024-12-09 15:43:22 -0500

    saving progress

---
+++ challenge3.py
@@ -0,0 +1,7 @@
+from scribe import Canvas
+from scribe import TerminalScribe
+
+# Challenge 3: Basic Data Structures
+# Create a Data Structure that holds information about scribes, including
+# starting positions, directions, and names.
+# Write a function that creates and moves scribes based on your data structure. 
